a0ca126d49bcae8b64fc2340d040672f9e59e509
Duarte David <deltaduartedavid@gmail.com>
2018-06-23 13:39:57 +0200

    duplicate

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ async function *read() {
     yield "Unicorn";
 }
 
-async function *openFile(path, { flags = 'r', mode = 0o666,  highWaterMark = 64 * 1024 } = {}) {
+async function *readFile(path, { flags = 'r', mode = 0o666,  highWaterMark = 64 * 1024 } = {}) {
     let handle;
     let read;
     try {
@@ -23,10 +23,77 @@ async function *openFile(path, { flags = 'r', mode = 0o666,  highWaterMark = 64 
     }
 }
 
+function writeFile(path, { flags = 'w', mode = 0o666 } = {}) {
+    return async input => {
+        let handle;
+        try {
+            handle = await fs.open(path, flags, mode)
+            for await (const buf of input) {
+                const len = Buffer.byteLength(buf)
+                let toWrite = len;
+                while(toWrite > 0) {
+                    toWrite -= (await handle.write(buf, len - toWrite, toWrite)).bytesWritten
+                }
+            }
+        } finally {
+            handle && await handle.close()
+        }
+    }
+}
+
+function *duplicate(input) {
+    let dups = 0
+    let nextElem
+    let resLock
+    let lock = new Promise(resolve => resLock = resolve)
+    let i = 0
+    let waiters = []
+
+    const sendToAll = async data => {
+        await lock
+        lock = new Promise(resolve => resLock = resolve)
+        waiters.forEach(([res]) => res(data))
+        waiters = []
+    }
+
+    const next = () => {
+        if (++i === dups)
+            resLock()
+        return new Promise((res, rej) => waiters.push([res, rej]))
+    }
+
+    void async function() {
+        try {
+            for await (const value of input) {
+               await sendToAll({ value })
+            }
+            await sendToAll({ done: true })
+        } catch(e) {
+            await lock
+            waiters.forEach(([, rej]) => rej(e))
+        }
+    }()
+
+    while(true) {
+        dups++
+        yield {
+            [Symbol.asyncIterator]() {
+                return this
+            },
+
+            next() {
+                return next()
+            },
+
+            close() {
+                dups--
+            }
+        }
+    }
+}
 
 async function *transform(input) {
     for await (const data of input) {
-        console.log('DATA@', data)
         yield data.toString().toUpperCase()
     }
 }
@@ -40,19 +107,11 @@ async function write(input) {
 }
 
 void async function () {
-/*    await (
-        read() |> transform |> write
-    )
-    console.log("End")
-
-    await do {
-        read() |> transform |> write
-    }
-    console.log("End2") */
-
-
-    await do {
-        openFile('package.json') |> transform |> write
-    }
+    const [s1, s2, s3] = readFile('package.json') |> transform |> duplicate
+    await Promise.all([
+        s1 |> write,
+        s2 |> writeFile('test'),
+        s3 |> writeFile('test2')
+    ])
     console.log('END')
 }()
